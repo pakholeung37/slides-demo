5b9748036985e6e20c40596de3ad791049147045
Pakho Leung <pakholeung37@gmail.com>
2020-12-19 18:33:24 +0800

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import { SASS } from "./components/sass"
 import { CSSModule } from "./components/css-module"
 import { CSSinJS } from "./components/css-in-js"
 import { AtomicCSS } from "./components/atomic-css"
-import { DesignSystem } from "./components/design-system"
-import { ChakraProvider } from "@chakra-ui/react"
+import { StyledSystem } from "./components/styled-system"
 function App() {
   return (
-    <ChakraProvider>
+    <>
       <h1 className="title">NativeCSS</h1>
       <NativeCSS />
       <h1 className="title">SASS</h1>
@@ -20,8 +19,8 @@ function App() {
       <h1 className="title">CSS in JS (emotion)</h1>
       <CSSinJS />
       <h1 className="title">Design System (chakra UI)</h1>
-      <DesignSystem />
-    </ChakraProvider>
+      <StyledSystem />
+    </>
   )
 }
 
