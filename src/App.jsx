0ab144374dbe406b5b2ac69e7d1fed41e0b5b5ec
Pakho Leung <pakholeung37@gmail.com>
2020-12-23 11:18:55 +0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ function App() {
       <h1 className="title">NativeCSS</h1>
       <NativeCSS />
       {/* <h1 className="title">SASS</h1>
-      <SASS />
-      <h1 className="title">CSS Module</h1>
-      <CSSModule />
-      <h1 className="title">AtomicCSS (tailwindcss)</h1>
-      <AtomicCSS />
-      <h1 className="title">CSS in JS (emotion)</h1>
-      <CSSinJS />
-      <h1 className="title">Design System (chakra UI)</h1>
+      <SASS /> */}
+      {/* <h1 className="title">CSS Module</h1>
+      <CSSModule /> */}
+      {/* <h1 className="title">AtomicCSS (tailwindcss)</h1>
+      <AtomicCSS /> */}
+      {/* <h1 className="title">CSS in JS (emotion)</h1>
+      <CSSinJS /> */}
+      {/* <h1 className="title">Styled System (chakra UI)</h1>
       <StyledSystem /> */}
     </>
   )
